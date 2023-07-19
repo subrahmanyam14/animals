6652884b005f155fa8f0a3a45cabac68abca2e42
subrahmanyam14 <139866081+subrahmanyam14@users.noreply.github.com>
2023-07-19 10:53:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function lion(){
+    document.getElementById("animal").src="https://images.unsplash.com/photo-1614027164847-1b28cfe1df60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGlvbiUyMGhlYWR8ZW58MHx8MHx8fDA%3D&w=1000&q=80";
+}
+function zebra(){
+    document.getElementById("animal").src="https://nationaltoday.com/wp-content/uploads/2022/02/International-Zebra-Day-1200x834.jpg";
+}
+function chithaa(){
+    document.getElementById("animal").src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/TheCheethcat.jpg/220px-TheCheethcat.jpg";
+}
